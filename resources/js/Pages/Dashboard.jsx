a197d9ff1adde7fb0c1c66fc9d020f500db581b7
SAURAV-FP <saurav@futureprofilez.com>
2023-11-06 09:51:25 +0530

    merge

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -58,9 +58,8 @@ export default function Dashboard(props) {
                             <div className='userManageRt mt-14'>
                                 <div className='userManageHead flex items-center justify-between mb-8'>
                                     <div>
-                                        <label for="country" className='mb-2 ms-1 text-white font-semibold'>Categories</label>
+                                        <label for="country" className='mb-2 ms-2 text-white font-semibold'>Categories</label>
                                         <select id="country" name="country" autocomplete="country-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                            <option value={'all'}>{'All'}</option>
                                             {categories && categories.map((c, i) => {
                                                 return <>
                                                     <option value={c.category}>{c.category}</option>
