8618b72fb611aa201cd3a38ae3de62dcb15c4c65
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2023-11-17 18:38:11 +0530

    latest changes added

--- resources/js/includes/ContentPrefrences.jsx
+++ resources/js/includes/ContentPrefrences.jsx
@@ -8,8 +8,7 @@ export default function ContentPrefrences(props){
   <Popup space='0' modalclass="pinkmodal" size="md"
    text={'Consent Preferences'} classes={`${props.classes} content-pre `}  >
       {loader ? 
-        <Loading /> 
-        : 
+        <Loading />  : 
         <div className="content-pr-modal" > 
           <iframe onLoad={()=>{UpdateLoader(false)}} src="https://app.termly.io/notify/696baafc-17cd-4a28-b758-a8f597cf2ad6" /> 
         </div> 
