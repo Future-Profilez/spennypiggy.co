3e34dd881d3cc9e79e313da8bc8aae34b7dd8151
naveentehrpariya <naveentehrpariya@gmail.com>
2023-11-21 18:08:56 +0530

    latest changes added

--- resources/js/wishlist/Wishlistbox.jsx
+++ resources/js/wishlist/Wishlistbox.jsx
@@ -10,8 +10,6 @@ import { useEffect } from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Wishlist from '@/Pages/Auth/Wishlist';
 
- 
-
 export default function Wishlistbox({ itm, itemid, auth, IsloggedIn, fetchingcats, categories }) {
 
   const [itemUID, setItemUID] = useState(itemid);
