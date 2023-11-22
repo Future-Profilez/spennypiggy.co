e951718642eec5efdbea531fb7e8b4627f802c90
SAURAV-FP <saurav@futureprofilez.com>
2023-11-22 15:18:38 +0530

    merg

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -165,9 +165,9 @@ class AuthenticatedSessionController extends Controller
                 // Username contains only lowercase letters, numbers, and underscores
                 $user = User::where('username', $username)->first();
                 if (!empty($user)) {
-                    return response()->json(['status' => true]);
-                } else {
                     return response()->json(['status' => false, 'msg' => 'Username is not available']);
+                } else {
+                    return response()->json(['status' => true, 'msg' => 'Username is available']);
                 }
             } else {
                 // Username contains spaces, special characters, or capital letters
