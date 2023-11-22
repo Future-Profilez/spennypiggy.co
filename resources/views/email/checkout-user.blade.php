110d8aa401a15ad39b79990957dfcafa2c91d8c4
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2023-11-22 18:23:51 +0530

    latest changes added s

--- resources/views/email/checkout-user.blade.php
+++ resources/views/email/checkout-user.blade.php
@@ -5,30 +5,26 @@
             <table width="100%" cellspacing="0" cellpadding="0" border="0"
                 style="max-width: 296px; width: 100%; text-align: center;">
                 <tr>
-                    <td
-                        style="font-family: Arial; font-weight: bold; font-size: 24px; color:#000; line-height: 32px; padding: 0 0 25px 0; text-align: center;">
-                        You have granted a wish on <br> Spenny Piggy 🎁 </td>
-                </tr>
-                <tr>
                     <td style=" padding: 0 0 25px 0; text-align: center;"><img src="https://whoyouinto.com/emails/user/giftimg.png" alt="img"></td>
                 </tr>
                 <tr>
                     <td
                         style="padding: 0 0 15px 0; font-family: Arial; font-weight: bold;  font-size: 18px; line-height: 27px;  color: 141414; text-align: left; text-align: center;">
-                        You granted <span style="color:#F94F97 ">{{ $data->owner->name }}'s Wish of
-                            £{{ $data->amount_subtotal }}</span> 🤩
+                            <span style="color:#F94F97 ">
+                            Thankyou for granted a wish of ({{ $data->owner->name }}'s) on  Spenny Piggy of amount £{{ $data->amount_subtotal }} 🎁 
+                            </span>
                     </td>
                 </tr>
                 <tr>
                     <td
                         style="padding: 0 0 20px 0; font-family: Arial; font-weight: normal; font-size: 14px; line-height: 22px; color: #4D4D4D; text-align: center; ">
-                        Go to <a href="#">Spenny Piggy</a> where you can see your granted wish, send a message to
+                        Go to <a href="spennypiggy.co">Spenny Piggy</a> where you can see your granted wish, send a message to
                         your gifter and share your gift on social media </td>
                 </tr>
                 <tr>
                     <td style="padding:0 0 10px 0; text-align: center;">
                         <a href={{ env('APP_URL') . '/' . $data->user->username }}
-                            style="padding:13px 10px 13px 10px; width: 210px; border:none;background-color: #F94F97; font-family: Arial; font-weight: bold; font-size: 15px; text-align: center; color:#ffffff; cursor: pointer;">See
+                            style="padding:13px 30px 13px 30px; width: 210px; text-decoration:none; border:none;background-color: #F94F97; font-family: Arial; font-weight: bold; font-size: 15px; text-align: center; color:#ffffff; cursor: pointer;">See
                             your granted wish</a>
                     </td>
                 </tr>
