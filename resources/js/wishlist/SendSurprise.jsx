c73582a5db7409c457d25a59a2b49f6f62fe59b3
saxin-chawla <sachinchawla24682@gmail.com>
2023-11-23 11:44:58 +0530

    merge

--- resources/js/wishlist/SendSurprise.jsx
+++ resources/js/wishlist/SendSurprise.jsx
@@ -25,17 +25,17 @@ export default function SendSurprise({owner}) {
             classes={`btn-pink lg px-4 w-100`}
             text={`Send Surprise`} >
             <h2 className="text-uppercase font-GillSans pb-4 font-large">Send a Surprise Gift</h2>
-      
+
 
             <div className="form-field mb-4">
                   <label className="d-block text-start mb-2" >Amount</label>
                   <input className="form-input w-100 rounded" onChange={(e) => setAmount(e.target.value)} type="text" placeholder="Enter" />
                   <p className="mt-1" >The amount is set to {format(amount)} GBP in the wisher's currency</p>
             </div>
-      
+
             <div className="form-field mb-4">
                   <label className="d-block text-start mb-2" >Suggested use (optional)</label>
-                  <textarea placeholder='Message...' className="form-input w-100 rounded" 
+                  <textarea placeholder='Message...' className="form-input w-100 rounded"
                   onChange={(e) => setMessage(e.target.value)} type="text" />
             </div>
 
@@ -44,7 +44,7 @@ export default function SendSurprise({owner}) {
                className="flex w-100 btn-pink lg mx-auto"
                spinnerClassName="fill-red-600" > {loading ? "Proccessing" : "Send Surprize Gift"}
             </LoaderButton>
-      
+
       </Popup>
     );
 }
