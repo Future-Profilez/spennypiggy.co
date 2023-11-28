4d54f4e7facc3302579af8838c88115632b968d3
saxin-chawla <sachinchawla24682@gmail.com>
2023-11-28 12:31:12 +0530

    merge

--- routes/auth.php
+++ routes/auth.php
@@ -16,7 +16,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\Auth\WishitemController;
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
-use App\Models\UserCart;
 use App\Models\WishItem;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -125,16 +124,17 @@ Route::middleware('auth')->group(function () {
     })->middleware(['auth', 'verified'])->name('stripe')->middleware('mustHaveToVerify');
 });
 
-Route::get('cart', [WishitemController::class, 'cartItems'])->name('cart');
+Route::get('cart', [WishitemController::class, 'cartItems'])->name('cart')->middleware('mustHaveToVerify');
 
 Route::get('user/{uuid}', [VerifyEmailController::class, 'emailVerify']);
 /*Anonymous checkout*/
 // Route::get('/anonymous-create-checkout-session/{priceid}/{quantity}', [StripeController::class, 'createAnonymousCheckout'])->name('anonymous.create.checkout');
 // Route::get('/anonymous-create-checkout-session/{wishid}/{amount?}', [StripeController::class, 'createAnonymousCheckout'])->name('anonymous.create.checkout');
 
-Route::post('/anonymous-create-checkout-session/{amount?}', [StripeController::class, 'createAnonymousCheckout'])->name('anonymous.create.checkout');
+Route::post('/anonymous-create-checkout-session/{wishid?}/{amount?}', [StripeController::class, 'createAnonymousCheckout'])->name('anonymous.create.checkout');
 
 Route::get('/anonymous-sucess-checkout/{id}/{cart_id?}', [StripeController::class, 'anonymousSuccessCheckout'])->name('checkout.anonymous.success');
+
 Route::get('/anonymous-cancel-checkout/{id}', [StripeController::class, 'anonymousCancelCheckout'])->name('checkout.anonymous.cancel');
 
 Route::get('/get_category_data/{category}/{user_id}', [WishitemController::class, 'categoryItems'])->name('get_category_data');
@@ -149,6 +149,11 @@ Route::get('/how-it-works', function () {
 Route::get('/terms-and-conditions', function () {
     return Inertia::render('Terms');
 })->name("terms-and-conditions");
+
+Route::get('/files/{filename}', function (string $filename) {
+    $fullPath = asset($filename);
+    return Storage::response($fullPath);
+});
 
 /*check username exist*/
 Route::get('check-username/{username}', [AuthenticatedSessionController::class, 'checkUserName'])->name('check.username');
