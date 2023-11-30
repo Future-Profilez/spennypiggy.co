888e227e54e231c8291d6efbd3752656a2589a63
saxin-chawla <sachinchawla24682@gmail.com>
2023-11-30 10:56:06 +0530

    merge

--- app/EmailService.php
+++ app/EmailService.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Mail\Checkout;
 use App\Mail\CheckoutToUser;
+use App\Mail\ForgotPassEmail;
 use App\Mail\SubscriptionMail;
 use App\Mail\VerifyEmail;
 use App\Mail\Welcome;
@@ -108,6 +109,17 @@ class EmailService
         try {
             Mail::to($data['to'])
                 ->send(new VerifyEmail($data));
+        } catch (TransportException $e) {
+            AppService::setStatus('email', 0, $e->getMessage());
+        }
+    }
+
+    public static function ForgotPassword($data)
+    {
+        try {
+            \Log::info('2 data:' . $data);
+            Mail::to($data['to'])
+                ->send(new ForgotPassEmail($data));
         } catch (TransportException $e) {
             AppService::setStatus('email', 0, $e->getMessage());
         }
---
+++ app/Mail/ForgotPassEmail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ForgotPassEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        try {
+            \Log::info('3 data:' . $this->data);
+            $subject = 'Forgot password email from spenny piggy platform.';
+            return $this->view('email.forgot-password')
+                ->from('Noreply@spennypiggy.co', 'SPENNY PIGGY')
+                ->subject($subject);
+        } catch (\Exception $e) {
+        }
+    }
+}
