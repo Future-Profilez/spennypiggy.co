cb124d7bb4e615d65468c219bc501cf119c62e52
naveentehrpariya <naveentehrpariya@gmail.com>
2023-11-30 23:08:21 +0530

    checkout added

--- resources/js/includes/LiveBar.jsx
+++ resources/js/includes/LiveBar.jsx
@@ -1,38 +1,53 @@
-export default function LiveBar(){
-   return <>
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const slideAnimation = keyframes`
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+`;
+
+const LiveBarWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+`;
+
+const ScrollingContainer = styled.div`
+  white-space: nowrap;
+  animation: ${slideAnimation} 18s linear infinite; 
+  display: flex;
+`;
+
+const ScrollingContent = styled.div`
+  display: flex;
+  margin-right: 2rem; 
+`;
+
+const LiveBar = () => {
+  return (
+    <LiveBarWrapper className="livebar mintbg py-3 pb-2 px-2">
       <style>{`
          .livebar p{font-size:18px;text-transform:uppercase;}
          @media(max-width:575px){
             .livebar p{font-size:15px;}
          }
       `}</style>
-      <div className="livebar mintbg py-3 pb-2 px-2" >
-         <marquee  direction="left" behavior="scroll" dir="rtl" 
-          hspace="10" loop scrollamount="4" scrolldelay="5" >
-            <div className="d-flex" >
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-               <p className="mb-0 mx-4 font-GillSans text-uppercase" >🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
-            </div>
-         </marquee>
-      </div>
-   </>
-}
+      <ScrollingContainer>
+        <ScrollingContent>
+          {/* Repeat your content here */}
+          <p className="mb-0 mx-3 font-GillSans text-uppercase">🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
+          <p className="mb-0 mx-3 font-GillSans text-uppercase">🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
+          <p className="mb-0 mx-3 font-GillSans text-uppercase">🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
+          <p className="mb-0 mx-3 font-GillSans text-uppercase">🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
+          <p className="mb-0 mx-3 font-GillSans text-uppercase">🚨KEEP 100% OF EVERYTHING YOU EARN 🚨</p>
+          {/* Repeat as many times as needed */}
+        </ScrollingContent>
+      </ScrollingContainer>
+    </LiveBarWrapper>
+  );
+};
+
+export default LiveBar;
