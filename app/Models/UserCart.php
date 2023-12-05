0c0c83be80eead98b3e0185cd7efc1578b8ebab7
saxin-chawla <sachinchawla24682@gmail.com>
2023-12-05 12:51:29 +0530

    merge

--- app/Models/UserCart.php
+++ app/Models/UserCart.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserCart extends Model
 {
-    use HasFactory;
-
+    use HasFactory, SoftDeletes;
+    protected $dates = ['deleted_at'];
     protected $fillable = [
         "uuid",
         "user_id",
@@ -22,6 +23,7 @@ class UserCart extends Model
         'priceid',
         'message',
         "status",
+        'deleted_at',
     ];
 
     public static function boot()
--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -14,5 +14,5 @@ export default function NavLink({ active = false, className = '', children, ...p
         >
             {children}
         </Link>
-    );
+    );  
 }
