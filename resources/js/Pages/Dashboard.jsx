d6951d1bbc5da216022303da37c0837a4bb0f04e
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2023-12-12 16:22:57 +0530

    latest added

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -48,7 +48,9 @@ export default function Dashboard(props) {
         (auth && auth.user && auth.user.username) == (user && user.username)
     );
 
-    
+    console.log("items",items)
+
+
     
     return (
         <Guest auth={auth.user} user={user}>
--- resources/js/includes/PinWish.jsx
+++ resources/js/includes/PinWish.jsx
@@ -1,12 +1,20 @@
+import { useAlerts } from '@/Components/Alerts';
 import axios from 'axios';
-export default function PinWish({text, id}){
+export default function PinWish({text, id, fetchingcats}){
    
+   const { successAlert, errorAlert } = useAlerts();
+
    const pin = (e) => {
       if(!id){ 
          return false;
       }
       axios.get(`/pin-item/${id}`).then((resp) => {
-         console.log("resp", resp);
+         if(resp.data.status){
+            successAlert(resp.data.msg);
+            fetchingcats && fetchingcats("all")
+         }else{
+            errorAlert(resp.data.msg)
+         }
       }).catch((_err) => {
          console.error("error", _err);
       });
