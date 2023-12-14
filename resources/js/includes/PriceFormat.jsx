0f92dbabdefba577976a5588444781f7b8e9c6bc
FP-DEV-PRADEEP <pradeep@fpdemo.com>
2023-12-14 21:32:50 +0530

    Currency Conversion Update

--- resources/js/includes/PriceFormat.jsx
+++ resources/js/includes/PriceFormat.jsx
@@ -1,3 +1,5 @@
+import { usePage } from "@inertiajs/react";
+
 export default function PriceFormat(){
 
    const format = ( amount ) => {
@@ -7,8 +9,37 @@ export default function PriceFormat(){
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
       }).format(amount);
-      return formattedPrice 
+      return formattedPrice
     };
 
-    return {format}
+    /**
+     * Format the Price in Multi-currency and Exchange Rate
+     *
+     * @param   {any}   amount Price Amount
+     * @param   {string} currency Price Currency
+     * @return {string}
+     */
+    const formatMultiPrice = (amount, currency = 'GBP') => {
+        const {rates, global_currency} = usePage().props;
+        // console.log('Global', global_currency);
+        if(rates[currency] || false){
+            const toGBP = rates[currency] * amount;
+            const toGlobal = toGBP * rates[global_currency];
+
+            return new Intl.NumberFormat('en-GB', {
+                style: 'currency',
+                currency: global_currency,
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            }).format(toGlobal);
+        }
+        return new Intl.NumberFormat('en-GB', {
+            style: 'currency',
+            currency: currency,
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(amount);
+    }
+
+    return {format, formatMultiPrice}
 }
