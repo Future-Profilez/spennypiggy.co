2e1fbe182be4e61b8c61e04b41f055a55483a56a
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2023-12-26 13:16:54 +0530

    Latest changes added

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -46,7 +46,6 @@ export default function Dashboard(props) {
         });
     }
 
-
     const [its, setIts] = useState();
     const fetchingcats = (e) => {
         setLoading(true);
@@ -64,7 +63,8 @@ export default function Dashboard(props) {
             console.error("error", error);
             setLoading(false);
         });
-};
+    };
+    console.log("its",its)
 
 
     const showCategory = (e) => {
@@ -78,9 +78,7 @@ export default function Dashboard(props) {
         fetchingLinks();
     }, []);
 
-
     const w = useWidthCount();
-
     const currencyaction = (e) => { 
         if(e == 'open'){
             setOpenCurrency(true)
@@ -88,58 +86,41 @@ export default function Dashboard(props) {
             setOpenCurrency(false)
         }
     }
+
     const [openCurrency, setOpenCurrency] = useState(null);
     useEffect(()=>{
         if(global_currency == null){
             setOpenCurrency(true)
         }
     });
+ 
 
-
-    const Items = () => { 
-        return <>
-        {!loading && its.map((c, i) => {
-            return <div key={`wish-item-${i}`} className="col-xl-4 col-lg-6 col-6">
-                <Wishlistbox
-                currency={global_currency}
-                fetchingcats={fetchingcats}
-                categories={categories}
-                IsloggedIn={IsloggedIn}
-                auth={auth.user}
-                itemid={itemid}
-                setuped={auth && auth.user && auth.user.stripe_details_submitted == 1 ? true : false}
-                itm={c}
-                key={`wish-${c.uuid}`} />
-            </div>
-        })}
-        </>
+    const updateMovement = async (updated) => { 
+        const array = [];
+        updated.forEach(name => {
+            array.push(name.id)
+        });
+        axios.post(`move-wish`, { 
+        shuffled_items: array })
+        .then((resp) => {
+        }).catch((_err) => {
+            console.error("error", _err);
+        });
     }
 
     const handleDragEnd = (event) => {
+        if(!IsloggedIn){
+            return false
+        }
         const { active, over } = event;
         const activeIndex = its.findIndex((item) => item.id === active.id);
-        const overIndex = over ? its.findIndex((item) => item.id === over.id) : -1;
-        if (activeIndex !== overIndex) {
-          const updated = arrayMove(its, activeIndex, overIndex, { key: 'id' });
+        const newOverIndex = over ? its.findIndex((item) => item.id === over.id) : null;
+        if (activeIndex !== newOverIndex) {
+          const updated = arrayMove(its, activeIndex, newOverIndex, { key: 'id' });
           setIts(updated);
+          updateMovement(updated)
         }
     };
-
-
-    // const WishItems = () => { 
-    //     return <>
-    //         {IsloggedIn  ? 
-    //             <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-    //                 <SortableContext strategy={rectSortingStrategy} items={its}>
-    //                         <Items />
-    //                 </SortableContext>
-    //             </DndContext>
-    //             : 
-    //             <Items />
-    //         }
-    //     </>
-    // }
-
 
 
     return <>
@@ -150,10 +131,10 @@ export default function Dashboard(props) {
                     <VersionUpdate />
                     {w > 991 ? <div className='wishbanner '>
                         <LazyLoadImage
-                            alt={"image"} useIntersectionObserver={true} effect="blur"
-                            height={390}
-                            className='w-full border-black border-2 shadow-mint rounded-2xl' src={user?.cover_url || wishlistbannerimg}
-                            width={1185} />
+                        alt={"image"} useIntersectionObserver={true} effect="blur"
+                        height={390}
+                        className='w-full border-black border-2 shadow-mint rounded-2xl' src={user?.cover_url || wishlistbannerimg}
+                        width={1185} />
                     </div> : ''}
                     <div className="wishManage">
                         <div className="row">
@@ -199,14 +180,8 @@ export default function Dashboard(props) {
                                                         </li>
                                                         <li>
                                                             <ShareProfile
-                                                                username={
-                                                                    user &&
-                                                                    user.name
-                                                                }
-                                                                classes={
-                                                                    "d-flex ms-auto"
-                                                                }
-                                                            >
+                                                                username={user && user.name}
+                                                                classes={"d-flex ms-auto"} >
                                                                 <svg
                                                                     width="24"
                                                                     height="25"
@@ -269,12 +244,27 @@ export default function Dashboard(props) {
                                     {loading ? <LoadingScreen /> : ""}
 
                                     
-                                    <div className="row items-lists">
+                                    <div className="row  items-lists">
                                         {IsloggedIn || user?.stripe_details_submitted == 1 ? (
                                                 <>
                                                     {its && its.length ? <>
-                                                        {/* <WishItems /> */}
-                                                        <Items />
+                                                        <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                                                            <SortableContext strategy={rectSortingStrategy} items={its}>
+                                                                {!loading && its.map((c, i) => {
+                                                                    return <Wishlistbox 
+                                                                            key={`wish-item-${i}`}
+                                                                            currency={global_currency}
+                                                                            fetchingcats={fetchingcats}
+                                                                            categories={categories}
+                                                                            IsloggedIn={IsloggedIn}
+                                                                            auth={auth.user}
+                                                                            itemid={itemid}
+                                                                            setuped={auth && auth.user && auth.user.stripe_details_submitted == 1 ? true : false}
+                                                                            itm={c}
+                                                                        />
+                                                                })}
+                                                            </SortableContext>
+                                                        </DndContext>
                                                     </>
                                                     : 
                                                         <>
