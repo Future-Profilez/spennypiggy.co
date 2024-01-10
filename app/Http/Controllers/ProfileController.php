71eb5dcfa2e53885b1ac32f38838852bfc2f62e0
SAURAV-FP <saurav@futureprofilez.com>
2024-01-10 16:46:00 +0530

    merge

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -190,12 +190,24 @@ class ProfileController extends Controller
 
         $media = $request->media;
 
-        $intro = UserIntro::create([
-            'uuid' => $media['uuid'],
-            'user_id' => Auth::id(),
-            'height' => $media['videoInfo']['video']['height'],
-            'width' => $media['videoInfo']['video']['width'],
-        ]);
+        $intro = UserIntro::where('user_id',Auth::id())->first();
+
+        if(empty($intro)){
+            $intro = UserIntro::create([
+                'uuid' => $media['uuid'],
+                'user_id' => Auth::id(),
+                'height' => $media['videoInfo']['video']['height'],
+                'width' => $media['videoInfo']['video']['width'],
+            ]);
+        }
+        else
+        {
+            $intro->uuid = $media['uuid'];
+            $intro->height = $media['videoInfo']['video']['height'];
+            $intro->width = $media['videoInfo']['video']['width'];
+            $intro->save();
+        }
+
         $intro->refresh();
 
         $intro->poster_url;
