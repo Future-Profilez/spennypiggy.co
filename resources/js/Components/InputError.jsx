e34916f2fce911462018a44fbe66333d3d2efb34
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-01-24 10:15:22 +0530

    latest changes added

--- resources/js/Components/InputError.jsx
+++
@@ -1,7 +0,0 @@
-export default function InputError({ message, className = '', ...props }) {
-    return message ? (
-        <span {...props} className={'text-sm text-red-600 ' + className}>
-            {message}
-        </span>
-    ) : null;
-}
