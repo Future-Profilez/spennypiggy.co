245bc09d6a4729fed63b256e0605fe01a065760b
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-01-31 17:18:11 +0530

    merge

--- app/Mail/Checkout.php
+++ app/Mail/Checkout.php
@@ -40,7 +40,7 @@ class Checkout extends Mailable
     public function build()
     {
         try {
-            $subject = 'You’ve just received a surprise gift on Spenny Piggy.';
+            $subject = 'You’ve just received a gift on Spenny Piggy.';
             return $this->view('email.checkout')
                 ->from('Noreply@spennypiggy.co', 'SPENNY PIGGY')
                 ->subject($subject);
--- app/Mail/CheckoutToUser.php
+++ app/Mail/CheckoutToUser.php
@@ -35,7 +35,7 @@ class CheckoutToUser extends Mailable
     public function build()
     {
         try {
-            $subject = 'Surprise Gift Granted on Spenny Piggy!';
+            $subject = 'Gift Granted on Spenny Piggy!';
             return $this->view('email.checkout-user')
                 ->from('Noreply@spennypiggy.co', 'SPENNY PIGGY')
                 ->subject($subject);
