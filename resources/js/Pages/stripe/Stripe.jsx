c794102d7860741a9d6ba8dbdb4f4432236cea15
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-02-05 18:43:30 +0530

    latest change added

--- resources/js/Pages/stripe/Stripe.jsx
+++ resources/js/Pages/stripe/Stripe.jsx
@@ -18,13 +18,18 @@ export default function Stripe(props) {
     const [countryCurrency, setCountryCurrency] = useState();
     const [country, setCountry] = useState();
     const getCountry = (e) => {
+        if (country == null || undefined) {
+            errorAlert("Please choose your country.");
+            return false;
+        }
         const name = JSON.parse(e);
         setCountry((name && name.code) || "");
         setCountryCurrency((name && name.currency) || "");
     };
 
     const checkTerms = () => {
-        if (country == "" || undefined) {
+        console.log("country", country);
+        if (country == null || undefined) {
             errorAlert("Please choose your country.");
             return false;
         }
