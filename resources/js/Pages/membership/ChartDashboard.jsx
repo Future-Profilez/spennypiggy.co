4dbbec5dc36779d165229a8f61d2dfe97af315dc
SAURAV-FP <saurav@futureprofilez.com>
2024-02-15 10:14:03 +0530

    merge

--- resources/js/Pages/membership/ChartDashboard.jsx
+++ resources/js/Pages/membership/ChartDashboard.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import{LineChart,Line,XAxis,YAxis,CartesianGrid,Tooltip,Legend,ResponsiveContainer,Customized,Cross,} from 'recharts';
-
+// import{LineChart,Line,XAxis,YAxis,CartesianGrid,Tooltip,Legend,ResponsiveContainer,Customized,Cross,} from 'recharts';
 
 export default function ChartDashboard() {
-
     const chartRef = useRef(null);
     const chartInstance = useRef(null);
     const [content, setContent] = useState([]);
@@ -12,25 +10,24 @@ export default function ChartDashboard() {
 
     const fetchdata = () => {
         setLoading(true);
-        axios.get(`/membership/graph`)
-        .then((res) => {
-            setContent(res.data.data);
-            setLoading(false);
-        })
-        .catch((err) => {
-            setLoading(false);
-        });
+        axios
+            .get(`/membership/graph`)
+            .then((res) => {
+                setContent(res.data.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setLoading(false);
+            });
     };
 
     useEffect(() => {
         fetchdata();
     }, []);
 
-    
-
     return (
         <div className="box rounded-lg mb-3 border p-3">
-            <h2 className="text-large font-bold mb-4 pt-2" >Earnings</h2>
+            {/* <h2 className="text-large font-bold mb-4 pt-2" >Earnings</h2>
             <ResponsiveContainer width="100%" height={500}>
             <LineChart
                 width={500}
@@ -50,9 +47,9 @@ export default function ChartDashboard() {
                 <Legend />
                 <Line type="monotone" dataKey="Amount" stroke="#8884d8" />
                 <Line type="monotone" dataKey="Time" stroke="#82ca9d" />
-            
+
             </LineChart>
-            </ResponsiveContainer>
+            </ResponsiveContainer> */}
         </div>
     );
 }
