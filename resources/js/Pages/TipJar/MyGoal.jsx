35d503e71e24b48a183ab7ac04eb0e5ae592eab3
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-04-18 18:12:44 +0530

    latest changes added

--- resources/js/Pages/TipJar/MyGoal.jsx
+++ resources/js/Pages/TipJar/MyGoal.jsx
@@ -23,7 +23,7 @@ export default function MyGoal({goal}) {
       </div>
       <div className='p-3' >
         <p className='mb-3 '>Total Earnings</p>
-        {goal.days ? <p className='mb-3 text-voilet '>{goal.days > 1 ? `${goal.days} Days` : `${goal.days} Day`} left to goal ends.</p> : ''}
+        {/* {goal.days ? <p className='mb-3 text-voilet '>{goal.days > 1 ? `${goal.days} Days` : `${goal.days} Day`} left to goal ends.</p> : ''} */}
         <ProgressBar now={goal?.fullfilled}  max={goal?.target} />
         <p className='text-muted text-small mt-2' >{getPercentage(goal?.target, goal?.fullfilled)}% of {formatMultiPrice(goal?.target, goal?.currency)} goal.</p>
       </div>
