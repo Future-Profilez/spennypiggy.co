c8ecd1f555989ac367e47c4c82ecf7f38b58adb3
SAURAV-FP <saurav@futureprofiez.com>
2024-04-24 16:30:56 +0530

    merge

--- routes/auth.php
+++ routes/auth.php
@@ -353,6 +353,10 @@ Route::prefix("wish")->name("wish.")->group(function () {
     Route::get('/handle/{uuid}/{status}', [StripeController::class, 'handleSubscription'])->name('subscribe.handle');
 });
 
+Route::get('payment/thankyou', function () {
+    return Inertia::render('Profile/Thankyou');
+})->name("thank-you");
+
 Route::prefix("membership")->name("membership.")->group(function () {
     Route::match(['get', 'post'], 'checkout/{uuid}/{reccure?}', [MembershipController::class, 'buyLevel'])->name("checkout");
     Route::get('/handle/{uuid}/{status}', [MembershipController::class, 'handlePayment'])->name('handle');
