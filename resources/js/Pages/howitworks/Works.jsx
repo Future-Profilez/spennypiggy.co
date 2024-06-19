ec2d8b21b9b279cbe000d42ac82ae20c8134d3cc
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-06-19 18:16:30 +0530

    chanhes added

--- resources/js/Pages/howitworks/Works.jsx
+++ resources/js/Pages/howitworks/Works.jsx
@@ -22,51 +22,55 @@ export default function Works(props) {
                 <p className='text-center'>Setting up your wishlist on Spenny Piggy only takes a few minutes. Add gifts from our <br/> partner brand catalog or any other retailer in the world.</p>
                 <div className='howWorkTab mt-12 pb-12 mx-auto'>
                     <Tabs defaultActiveKey="1" id="uncontrolled-tab-example" className="mb-3">
-                        <Tab eventKey="1" title="for Gifters">
+                        <Tab eventKey="1" title="Supporters">
                             <div className='funboxs mintbg shadow-black border-black mb-10'>
                                 <div className='funboximg' data-aos="fade-up" >
-                                    <img src={vishitimg01} alt='img' />
-                                    </div>
-                                    <div className='funcnt'>
+                                        <img src={vishitimg01} alt='img' />
+                                </div>
+                                <div className='funcnt'>
                                     <h5 className='text-voilet mb-2'>STEP 1</h5>
-                                    <h3 className='headingSm text-shadow-black mb-3'>Visit A <br/> Wishlist</h3>
-                                    <p className='text-CeraGR'>Browse your favorite creator's wishes on their wishlist. From items, to outing, to treats, you can see everything your creator wishes for and add them to your gift basket.</p>
+                                    <h3 className='headingSm text-shadow-black mb-3'>Visit a <br></br> Creator’s Page</h3>
+                                    <p className='text-CeraGR'>Browse your favorite creator’s page and check out their latest posts, shop
+                                    items, wishes, and so much more.</p>
                                 </div>
                             </div>
                             
                             <div className='funboxs pinkbg shadow-black border-black mb-10'>
                                 <div className='funcnt' data-aos="fade-up">
                                     <h5 className='text-voilet text-black mb-2'>STEP 2</h5>
-                                    <h3 className='headingSm text-shadow-black mb-3 text-purple'>Create a Gift <br/>  Basket</h3>
-                                    <p className='text-CeraGR text-wh'>Pick one or more items to add to your gift basket</p>
+                                    <h3 className='headingSm text-shadow-black mb-3 text-purple'>Send Some<br/> Support</h3>
+                                    <p className='text-CeraGR text-wh'>Join a membership, purchase a wish or shop item, and maybe just send
+                                    some love to fill their Piggy Bank.</p>
                                 </div>
                                 <div className='funboximg'>
                                     <img src={giftbasketimg01} alt='img' />
                                 </div>
                             </div>
-
                             <div className='funboxs bluebg shadow-black border-black mb-10'>
-                                <div className='funboximg' >
-                                    <img src={fundbasketimg01} data-aos="fade-up" alt='img' />
+                                <div className='funboximg md:pe-4' >
+                                    <img src={fundbasketimg01} className='max-h-[400px]' data-aos="fade-up" alt='img' />
                                 </div>
-                                    
                                 <div className='funcnt' data-aos="fade-up" >
                                     <h5 className='text-mint mb-2'>STEP 3</h5>
-                                    <h3 className='headingSm text-shadow-black mb-3 text-pink'>Fund Basket <br/> with Message</h3>
-                                    <p className='text-CeraGR text-wh'>You can choose to leave a message and a pseudonym. Your email will be kept hidden, but we will relay any picture messages from the creator to this email.</p>
+                                    <h3 className='headingSm text-shadow-black mb-3 text-pink'>Create an <br></br> Account </h3>
+                                    <p className='text-CeraGR text-wh'>Sign up for your supporter account. At the end of checkout, you’ll be
+                                    prompted to create an account if you haven’t done so already. Here you can
+                                    find all your exclusive content purchases or check the status of your custom
+                                    orders.</p>
                                 </div>
                             </div>
                         </Tab>
-                        <Tab eventKey="2" title="for wishers">
+                        <Tab eventKey="2" title="Creators">
                         <div className='funboxs mintbg shadow-black border-black mb-10'>
-                                <div className='funboximg'>
-                                    <img src={yourwishlist01}  data-aos="fade-up" alt='img' />
+                                <div className='funboximg pe-4'>
+                                        <img src={yourwishlist01}  data-aos="fade-up" alt='img' />
                                     </div>
-                                    
                                     <div className='funcnt' data-aos="fade-up" >
                                     <h5 className='text-voilet mb-2'>STEP 1</h5>
-                                    <h3 className='headingSm text-shadow-black mb-3'>Create Your <br/> Wishlist</h3>
-                                    <p className='text-CeraGR'>Add items from any online store or manually add offline wishes. With our custom gift entry, you can get creative. List full outfits, trips to the spa, shopping sprees, and more.</p>
+                                    <h3 className='headingSm text-shadow-black mb-3'>Set Up Your Page</h3>
+                                    <p className='text-CeraGR'>Craft your unique space to sell exclusive content and custom products.<br></br>
+                                        Publish an epic reward-based Wishlist or offer awesome tailored
+                                        memberships. Make your page as unique as you are!</p>
                                 </div>
                             </div>
                             
@@ -74,7 +78,9 @@ export default function Works(props) {
                                 <div className='funcnt' data-aos="fade-up">
                                     <h5 className='text-voilet text-black mb-2'>STEP 2</h5>
                                     <h3 className='headingSm text-shadow-black mb-3 text-purple'>Set up your <br/> payments</h3>
-                                    <p className='text-CeraGR text-wh'>Using our secure established third party payment processor, set up your payments. This information is never seen by your gifter.</p>
+                                    <p className='text-CeraGR text-wh'>Using our secure third-party payment processor, set up your payments to
+                                    quickly and securely receive direct support and start funding your fabulous
+                                    lifestyle!</p>
                                 </div>
                                 <div className='funboximg'>
                                     <img src={setuppaymentimg01} data-aos="fade-up" alt='img' />
@@ -88,8 +94,10 @@ export default function Works(props) {
                                     
                                 <div className='funcnt' data-aos="fade-up">
                                     <h5 className='text-mint mb-2'>STEP 3</h5>
-                                    <h3 className='headingSm text-shadow-black mb-3 text-pink'>Share links to <br/> different <br/> platforms </h3>
-                                    <p className='text-CeraGR text-wh'>Showcase your gift with a shout-out on your socials or thank your fans directly on Spenny Piggy via a personal text or video message.</p>
+                                    <h3 className='headingSm text-shadow-black mb-3 text-pink'> 
+                                    Don’t Be Shy…</h3>
+                                    <p className='text-CeraGR text-wh'>Update your supporters with your first member-only posts. Share your page
+                                    across all your socials, set up auto tweets, and watch the support roll in.</p>
                                 </div>
                             </div>  
                         </Tab>
--- resources/js/Pages/leaderboard/Board.jsx
+++ resources/js/Pages/leaderboard/Board.jsx
@@ -8,11 +8,14 @@ import LargestGifts from "./LargestGifts";
 import { crown } from "@/includes/Icons";
 
 export default function Board(props) {
-    const { auth, data } = props;
+    const { auth, data, is_daily } = props;
+
+    console.log(props)
 
     const [positions, setPositions] = useState([]);
     const [ranks, setRanks] = useState([]);
 
+    console.log("props leaderboard", props);
     const filterPositions = (d) => {
         const newData = [...d];
         const positionsData = newData.slice(0, 3);
@@ -70,17 +73,37 @@ export default function Board(props) {
         return (
             <>
                 {p && p.username ? (
-                    <Link href={p && p.username} className={`position-${position} position text-center rounded-lg shadow-pink bg-white`} >
+                    <Link
+                        href={p && p.username}
+                        className={`position-${position} position text-center rounded-lg shadow-pink bg-white`}
+                    >
                         <div className="profile p-3 pb-0">
                             <div className="position-relative">
-                                    {position == 1 ? <div className="crown-wings" dangerouslySetInnerHTML={{ __html: crown }} /> : ''}
+                                {position == 1 ? (
+                                    <div
+                                        className="crown-wings"
+                                        dangerouslySetInnerHTML={{
+                                            __html: crown,
+                                        }}
+                                    />
+                                ) : (
+                                    ""
+                                )}
                                 <div className="profile-image ">
-                                    <img src={(p && p.avatar) || userphoto} className="img-fluid" alt="image" />
+                                    <img
+                                        src={(p && p.avatar) || userphoto}
+                                        className="img-fluid"
+                                        alt="image"
+                                    />
                                 </div>
                             </div>
                             <div className="profile-content">
-                                <h2 className="font-bold text-large pt-2">{(p && p.name) || "Anonymous"}</h2>
-                                <p className="toppercentage text-center">{p && p.top}% </p>
+                                <h2 className="font-bold text-large pt-2">
+                                    {(p && p.name) || "Anonymous"}
+                                </h2>
+                                <p className="toppercentage text-center">
+                                    {p && p.top}%{" "}
+                                </p>
                             </div>
                             <div className={`rank-position `}>
                                 <h2 className="font-GillSans">{position}</h2>
@@ -88,13 +111,28 @@ export default function Board(props) {
                         </div>
                     </Link>
                 ) : (
-                    <div className={`position-${position} position text-center rounded-lg shadow-pink bg-white`} >
+                    <div
+                        className={`position-${position} position text-center rounded-lg shadow-pink bg-white`}
+                    >
                         <div className="profile p-3 pb-0">
                             <div className=" position-relative">
-                                    {position == 1 ? <div  className="crown-wings" dangerouslySetInnerHTML={{ __html: crown }} /> : ''}
-                                    <div className="profile-image ">
-                                        <img src={(p && p.avatar) || userphoto} className="img-fluid" alt="image" />
-                                    </div>
+                                {position == 1 ? (
+                                    <div
+                                        className="crown-wings"
+                                        dangerouslySetInnerHTML={{
+                                            __html: crown,
+                                        }}
+                                    />
+                                ) : (
+                                    ""
+                                )}
+                                <div className="profile-image ">
+                                    <img
+                                        src={(p && p.avatar) || userphoto}
+                                        className="img-fluid"
+                                        alt="image"
+                                    />
+                                </div>
                             </div>
                             <div className="profile-content">
                                 <h2 className="font-bold text-large pt-2">
@@ -123,26 +161,43 @@ export default function Board(props) {
                         <div className="col-lg-8 mb-4">
                             <div className="pe-md-4">
                                 <div className="pt-4 pt-md-0 d-block d-md-flex align-items-center justify-content-between mb-4 pb-4">
-                                    <h1 className="text-bl font-GillSans text-start text-2xl uppercase text-white ">Leaderboard</h1>
+                                    <h1 className="text-bl font-GillSans text-start text-2xl uppercase text-white ">
+                                        Leaderboard
+                                    </h1>
                                     <div className="changePeriod">
                                         <button
-                                            className={period == "all" ? "active":""}
-                                            onClick={() => switchTime("all")} >
+                                            className={
+                                                period == "all" ? "active" : ""
+                                            }
+                                            onClick={() => switchTime("all")}
+                                        >
                                             All Time
                                         </button>
                                         <button
-                                            className={period == "monthly" ? "active":""}
-                                            onClick={() => switchTime("monthly")} >
+                                            className={
+                                                period == "monthly"
+                                                    ? "active"
+                                                    : ""
+                                            }
+                                            onClick={() =>
+                                                switchTime("monthly")
+                                            }
+                                        >
                                             Monthly
                                         </button>
-                                        <button className={period == "weekly" ? "active":""} onClick={() => switchTime("weekly")}>
+                                        <button
+                                            className={
+                                                period == "weekly"
+                                                    ? "active"
+                                                    : ""
+                                            }
+                                            onClick={() => switchTime("weekly")}
+                                        >
                                             Weekly
                                         </button>
-                                        <button
-                                            className={period == "daily"? "active": ""}
-                                            onClick={() => switchTime("daily")}>
-                                            Daily
-                                        </button>
+                                       {is_daily == 1 ? <button className={period == "daily"? "active": ""}
+                                            onClick={() => switchTime("daily")}>Daily
+                                        </button> : ''}
                                     </div>
                                 </div>
                                 <div
