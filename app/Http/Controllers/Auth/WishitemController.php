ebda8c8d5fccddccae4dd8326c3342235cc9df17
Naveen Tehrpariya <naveentehrpariya@gmail.com>
2025-03-03 14:57:56 +0530

    checking error

--- app/Http/Controllers/Auth/WishitemController.php
+++ app/Http/Controllers/Auth/WishitemController.php
@@ -1030,7 +1030,7 @@ class WishitemController extends Controller
             $ryeProductPayment->customer_email = $orderDetails->user->email;
             $ryeProductPayment->save();
 
-            Session::put('cartData', $orderDetails);
+            // Session::put('cartData', $orderDetails);
 
             $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
             $sessionCreate = $stripe->checkout->sessions->create([
