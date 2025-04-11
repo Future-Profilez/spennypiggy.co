c46df8d3016e13ab003dbd5a468b3aa548be2a7d
Prem Jangid <prem@futureprofilez.com>
2025-04-11 09:50:38 +0530

    update social media update functionality and reward section remove

--- resources/js/Pages/Auth/Social.jsx
+++ resources/js/Pages/Auth/Social.jsx
@@ -96,7 +96,7 @@ export default function Social({
                             Social Links
                         </h2>
                         {type == 'membership'  ? <p className="text-yellow-500 mb-4">
-                            Please add at least one social media handles. We will share your social media handles to the creator so they can chat with you.
+                            Please add at least one social media handle. We will share your social media handles to the creator so they can chat with you.
                         </p> : ''}
                         <form onSubmit={createSocial}>
                             <ul className=" ps-0  row">
