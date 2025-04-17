ac39b7ba7322c4112fdf4b156daa63509e575efe
Prem Jangid <prem@futureprofilez.com>
2025-04-17 15:25:19 +0530

    merge

--- resources/js/wishlist/Userprofile.jsx
+++ resources/js/wishlist/Userprofile.jsx
@@ -12,7 +12,7 @@ export default function Userprofile({ IsloggedIn }) {
 
     return (
         <div className="userprofilesec mb-4 ">
-             
+
             <div className="userPr px-4 py-0 py-md-4  d-flex align-items-center justify-content-between">
                 <div className="update-profile d-flex align-items-center justify-content-start" >
                     <div className="userphoto relative">
@@ -21,14 +21,14 @@ export default function Userprofile({ IsloggedIn }) {
                         src={user && user.avatar_url ? user.avatar_url : userphoto}
                         height={200} width={200} />
 
-                        {IsloggedIn && auth && auth?.user.avatar_url && auth?.user?.avatar_approved == 0 ? 
+                        {IsloggedIn && auth && auth?.user.avatar_url && auth?.user?.avatar_approved == 0 ?
                             <div className="absolute approvetag top-3 mx-auto">
                                 <button className='tooltipbtn' >
                                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M9 15H11V9H9V15ZM10 7C10.2833 7 10.521 6.904 10.713 6.712C10.905 6.52 11.0007 6.28267 11 6C11 5.71667 10.904 5.47933 10.712 5.288C10.52 5.09667 10.2827 5.00067 10 5C9.71667 5 9.47933 5.096 9.288 5.288C9.09667 5.48 9.00067 5.71733 9 6C9 6.28333 9.096 6.521 9.288 6.713C9.48 6.905 9.71733 7.00067 10 7ZM10 20C8.61667 20 7.31667 19.7373 6.1 19.212C4.88333 18.6867 3.825 17.9743 2.925 17.075C2.025 16.175 1.31267 15.1167 0.788 13.9C0.263333 12.6833 0.000666667 11.3833 0 10C0 8.61667 0.262667 7.31667 0.788 6.1C1.31333 4.88333 2.02567 3.825 2.925 2.925C3.825 2.025 4.88333 1.31267 6.1 0.788C7.31667 0.263333 8.61667 0.000666667 10 0C11.3833 0 12.6833 0.262667 13.9 0.788C15.1167 1.31333 16.175 2.02567 17.075 2.925C17.975 3.825 18.6877 4.88333 19.213 6.1C19.7383 7.31667 20.0007 8.61667 20 10C20 11.3833 19.7373 12.6833 19.212 13.9C18.6867 15.1167 17.9743 16.175 17.075 17.075C16.175 17.975 15.1167 18.6877 13.9 19.213C12.6833 19.7383 11.3833 20.0007 10 20Z" fill="#FF8E25"/>
                                 </svg>
                             <p>Profile avatar is waiting for approval. Currently only you can see this.</p></button>
-                            </div> 
+                            </div>
                         : ""}
 
                     </div>
@@ -46,17 +46,17 @@ export default function Userprofile({ IsloggedIn }) {
                         </div>
                     </div>
                 </div>
-                
 
-                {!IsloggedIn ? user && user.stripe_details_submitted == 1 && 
+
+                {!IsloggedIn ? user && user.stripe_details_submitted == 1 &&
                     <div>
-                        {user && user.role == 1 ? 
+                        {user && user.role == 1 ?
                         <>
                             <SendTip  />
                             {user && user.role == 1 ? <p className="text-center text-mint" >🐷 {supporters} Supporters</p> : ''}
                         </> : ''}
                     </div>
-                    :  
+                    :
                     <div>
                     <EditProfile
                         user={user}
@@ -65,7 +65,7 @@ export default function Userprofile({ IsloggedIn }) {
                         {user && user.role == 1 ? <p className="text-center text-mint mt-2" >🐷 {supporters} Supporters</p> : ''}
                     </div>
                 || ''}
-                
+
             </div>
         </div>
     );
