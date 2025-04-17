2ef592292c56ee415f27a4b672fd40455fbb7288
Prem Jangid <prem@futureprofilez.com>
2025-04-17 17:21:19 +0530

    update stripe account page for creator

--- routes/auth.php
+++ routes/auth.php
@@ -25,6 +25,8 @@ use App\Http\Controllers\Auth\WishitemController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WishtenderController;
 use App\Http\Middleware\VerifyCsrfToken;
+use App\Models\Logs;
+use App\Models\SocialLinks;
 use App\Models\TipGoalsPayment;
 use App\Models\User;
 use App\Models\WishItem;
@@ -166,7 +168,11 @@ Route::middleware('auth')->group(function () {
             Route::get('/read-status/{payment_id}/{type}', [WishitemController::class, 'readStatus'])->name('read-status');
 
             Route::get('/stripe', function () {
-                return Inertia::render('stripe/Stripe');
+                return Inertia::render('stripe/Stripe',[
+                    'social_media_status' => SocialLinks::where('user_id', Auth::user()->id)->exists() ? 1 : 0,
+                    'user_profile_status' => Auth::user()->avatar_approved,
+                    'bio_status' => !empty(Auth::user()->bio) ? 1 : 0,
+                ]);
             })->middleware(['auth', 'verified'])->name('stripe');
 
             Route::get('/pin-item/{wish_id}/', [WishitemController::class, 'pinItem'])->name('pin-item');
