85c739d9ea9c5eb738ef5ff40add127223f7c659
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2025-05-01 10:55:15 +0530

    merge changes

--- resources/js/Pages/home/LiveBarSection.jsx
+++ resources/js/Pages/home/LiveBarSection.jsx
@@ -19,7 +19,7 @@ export default function LiveBarSection() {
       src={HeroWishlistImage}
       alt="Wishlist image"
       />
-    <p className='uppercase text-center mt-1.5 text-white font-poppins text-[10px]'>*3 days Free trial and then requires a monthly £4 payment to cover stripe fees and compliance costs. </p>
+    <p className='uppercase text-center mt-1.5 text-white font-poppins text-[10px] xl:text-[15px]'>*3 days Free trial and then requires a monthly £4 payment to cover stripe fees and compliance costs. </p>
     {/* <div className='d-flex flex-wrap justify-content-center mt-4 align-items-center creators-platforms' >
         <div data-aos="zoom-in" className='px-4 py-2' >
           <LazyLoadImage
