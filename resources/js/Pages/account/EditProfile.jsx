7da332c6a4385d5b9bcdc534e43fc74a8a04a715
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2025-07-03 18:25:48 +0530

    profile image added

--- resources/js/Pages/account/EditProfile.jsx
+++ resources/js/Pages/account/EditProfile.jsx
@@ -9,6 +9,7 @@ import UpdateAvatar from './UpdateAvatar';
 import LoaderButton from '@/Components/LoaderButton';
 import PriceFormat from '@/includes/PriceFormat';
 import html2canvas from 'html2canvas';
+import { useEffect } from 'react';
 
 export default function EditProfile({ user, text, classes, updateProfileSteps }) {
  
@@ -18,6 +19,24 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
     const [coverImage, setCoverImage] = useState();
 
     const [socialFile, setSocialFile] = useState();
+    console.log("socialFile",socialFile);
+
+    useEffect(() => {
+        if (socialFile) {
+            setData('social_image', socialFile);
+        }
+    },[socialFile]);
+
+     const { data, setData, post, processing, errors, reset } = useForm({
+        name: user?.name || '',
+        username: user?.username || '',
+        bio: user?.bio || '',
+        avatar: '',
+        cover: '',
+        min_surprise_amount: user?.min_surprise_amount || '',
+        social_image: socialFile || null,
+    });
+
     const generateCardAndUpload = async (avataruid) => {
         const container = document.createElement('div');
         container.style.position = 'absolute';
@@ -26,7 +45,7 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
         container.style.zIndex = '-1';
         document.body.appendChild(container);
 
-        const nameuper = user?.name.toUpperCase();
+        const nameuper = data?.name ? data?.name.toUpperCase() : user?.name.toUpperCase();
         console.log("avataruid",avataruid)
         container.innerHTML = `
             <div id="card-to-capture" style="
@@ -53,7 +72,7 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
                         <p style="margin: 4px 0;font-family: cursive;font-size: 19px;">is now on <b style="
             font-weight: 800;
         ">🎁 SpennyPiggy</b></p> 
-                        <p style="font-size: 16px;font-family: system-ui;margin-top: 20px;">https://spennypiggy.co/${user?.username}</p>
+                        <p style="font-size: 16px;font-family: system-ui;margin-top: 20px;">https://spennypiggy.co/${data?.username || user?.username}</p>
                     </div>
                     </div>
         `;
@@ -73,11 +92,14 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
         const blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
         console.log("blob",blob)
         if (!blob) {
-            console.error('❌ Failed to convert card to image');
+            console.log('❌ Failed to convert card to image');
             return;
         }
-        setSocialFile(new File([blob],  `${user?.username}-social_avatar`, { type: blob.type }))
-        setData('social_image', new File([blob], `${user?.username}-social_avatar`, { type: blob.type }));
+        setTimeout(() => {
+            setSocialFile(new File([blob],  `${user?.username}-social_avatar`, { type: blob.type }))
+            setData('social_image', new File([blob], `${user?.username}-social_avatar`, { type: blob.type }));
+        },500);
+        return true;
         // 6. Download to user device
         // const localUrl = URL.createObjectURL(blob);
         // const a = document.createElement('a');
@@ -105,23 +127,14 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
 
     const [username, setUsername] = useState(user?.username);
 
-    const { data, setData, post, processing, errors, reset } = useForm({
-        name: user?.name || '',
-        username: user?.username || '',
-        bio: user?.bio || '',
-        avatar: '',
-        cover: '',
-        min_surprise_amount: user?.min_surprise_amount || '',
-        social_image: socialFile || null,
-    });
+   
+    console.log("data",data)
 
     const updateProfile = async (e) => {
         e.preventDefault();
-        if(user.avatar && user?.social_image == null){
-            await generateCardAndUpload(user.avatar);
-        }
+        
          
-        post(route('edit-profile'), {
+        post(route('edit-profile', {data}), {
             preserveScroll: true,
             onSuccess: (resp) => {
                 setClose(false);
@@ -153,6 +166,12 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
 
     const defaultCurrency = user.default_currency;
 
+
+    const IsProfileChannged = async() => {
+        if(user && user.avatar && !user?.social_image){
+            await generateCardAndUpload(user.avatar);
+        }
+    }
     return (
         <Popup modalclass='pinkmodal editprofile full' size='md' action={close}
             text={text||<> Update Profile </>}
@@ -173,13 +192,13 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
                     <ul>
                         <li className="mb-3">
                             <label className="mb-1">Display Name</label>
-                            <input type="text" name="name" defaultValue={user?.name || ''}
+                            <input onBlur={IsProfileChannged} type="text" name="name" defaultValue={user?.name || ''}
                                 onChange={(e) => setData('name', e.target.value)}
                                 className="form-input px-2 py-2 border w-full rounded-md" />
                         </li>
                         <li className="mb-2">
                             <label className="mb-1">Username</label>
-                            <input defaultValue={user?.username || ''} onChange={(e) => setData("username", e.target.value)}
+                            <input onBlur={IsProfileChannged} defaultValue={user?.username || ''} onChange={(e) => setData("username", e.target.value)}
                                 type="text" name="username" className="form-input px-2 py-2 border w-full rounded-md" placeholder='Spennypiggy.co/warner99' onKeyUp={(e) => {setUsername(e.target.value)}}/>
                         </li>
 
@@ -187,7 +206,7 @@ export default function EditProfile({ user, text, classes, updateProfileSteps })
 
                         <li className="mb-3">
                             <label className="mb-1">Bio</label>
-                            <textarea defaultValue={user?.bio || ''}
+                            <textarea onBlur={IsProfileChannged} defaultValue={user?.bio || ''}
                                 onChange={(e) => setData("bio", e.target.value)}
                                 name="bio" className="form-input px-2 py-2 border w-full rounded-md"
                                 placeholder='Bio' />
