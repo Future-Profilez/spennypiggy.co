b270d57fca3eb2cef68fa22b697668ec3ccff4bc
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2025-07-16 18:19:35 +0530

    profile page speed optimizations added

--- resources/js/wishlist/Userprofile.jsx
+++ resources/js/wishlist/Userprofile.jsx
@@ -20,7 +20,7 @@ export default function Userprofile({ IsloggedIn }) {
             <div className="userPr px-4 py-0 py-md-4 lg:flex items-center justify-center lg:justify-between mt-[-80px] md:mt-[-50px]">
                 <div className="update-profile text-center lg:flex items-center justify-center lg:justify-start">
                     <div className="userphoto relative !flex  items-center justify-center mb-4 ">
-                        <LazyLoadImage
+                        <LazyLoadImage fetchpriority="high"
                             alt={"image"}  effect="blur"
                             src={user && user.avatar_url? user.avatar_url: userphoto}
                             height={150} width={150}
