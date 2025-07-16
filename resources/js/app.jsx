e35e15ac67dede3fe7c070ecf4f379bf49488147
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2025-07-16 16:01:59 +0530

    env changes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -13,7 +13,7 @@ import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { Provider } from "react-redux";
 import store from "./Pages/redux/Store";
 import * as Sentry from "@sentry/react";
-// if(import.meta.env.VITE_APP_ENV == 'production'){
+if(import.meta.env.VITE_APP_ENV == 'production'){
     console.log("sentry enabled");
     Sentry.init({
       dsn: "https://14cda094324469c174a7e04a2298502d@o4509650305679360.ingest.us.sentry.io/4509650314526720",
@@ -28,9 +28,9 @@ import * as Sentry from "@sentry/react";
       replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
       replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     });
-// } else {
-//     console.log("sentry Disabled");
-// }
+} else {
+    console.log("sentry Disabled");
+}
 
 createInertiaApp({
     title: (title) =>
