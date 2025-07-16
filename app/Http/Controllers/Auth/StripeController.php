10d48776bc579d4e1bad3c4d93a6ab5d11a3401c
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2025-07-16 16:46:32 +0530

    env changes

--- app/Http/Controllers/Auth/StripeController.php
+++ app/Http/Controllers/Auth/StripeController.php
@@ -1234,7 +1234,6 @@ class StripeController extends Controller
                         "support_type" => 'leaderboard_unlock',
                         "guest_name" => $request->name ?? null,
                         "user_id" => Auth::id() ?? null,
-                        "user_profile" => env('APP_URL'). '/' . Auth::user()->username ?? null,
                         "creator_id" => $creator->id,
                         "creator_profile" => env('APP_URL'). '/' . $creator->username ?? null,
                     ],
