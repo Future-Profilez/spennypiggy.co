590a169977165b259e139b106c61bfed1a44cd92
Prem Jangid <prem@futureprofilez.com>
2025-08-01 13:01:33 +0530

    update wish pay amount when paying

--- app/Http/Controllers/Auth/CheckoutController.php
+++ app/Http/Controllers/Auth/CheckoutController.php
@@ -137,7 +137,7 @@ class CheckoutController extends Controller
                         'price_data' => [
                             'currency' => $currency,
                             'product' => $dd->wish_item_id == null || (isset($dd->wish->subscription) && ($dd->wish->subscription == 2)) ? $dd->priceid : $dd->wish->stripe_product_id,
-                            'unit_amount_decimal' => round($ConvertedToGBpAmount * 100),
+                            'unit_amount_decimal' => round($totalAmount * 100),
                         ]
                     ],
                     // Platform fee + Vat as a separate item
