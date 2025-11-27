5a5f5c7146085b94e151bd0068d8cacfbe39a281
Naveen Tehrpariya <naveentehrpariya@MacBook-Air-2.local>
2025-11-27 18:53:43 +0530

    merge changes

--- resources/js/includes/Footer.jsx
+++ resources/js/includes/Footer.jsx
@@ -1,57 +1,12 @@
-import { Head, Link, usePage } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
 import { useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import spennypiggy from "../../assets/img/logo.png";
 
 export default function Footer(props) {
     const { auth } = props;
-    const { props: pageProps } = usePage();
-    const intercom = pageProps?.intercom || {};
    
     async function configIntercom() {
-        setTimeout(() => {
-            const appId = intercom?.appId || "xomg14o9";
-            const boot = intercom?.boot || {
-                api_base: "https://api-iam.intercom.io",
-                app_id: appId,
-                custom_launcher_selector: ".livechat",
-            };
-            window.intercomSettings = boot;
-            (function () {
-                var w = window;
-                var ic = w.Intercom;
-                if (typeof ic === "function") {
-                    ic("reattach_activator");
-                    ic("update", w.intercomSettings);
-                } else {
-                    var d = document;
-                    var i = function () {
-                        i.c(arguments);
-                    };
-                    i.q = [];
-                    i.c = function (args) {
-                        i.q.push(args);
-                    };
-                    w.Intercom = i;
-                    var l = function () {
-                        var s = d.createElement("script");
-                        s.type = "text/javascript";
-                        s.async = true;
-                        s.defer = true;
-                        s.src = `https://widget.intercom.io/widget/${appId}`;
-                        var x = d.getElementsByTagName("script")[0];
-                        x.parentNode.insertBefore(s, x);
-                    };
-                    if (document.readyState === "complete") {
-                        l();
-                    } else if (w.attachEvent) {
-                        w.attachEvent("onload", l);
-                    } else {
-                        w.addEventListener("load", l, false);
-                    }
-                }
-            })();
-        }, 1000);
     }
 
     async function confgureGtag() {
@@ -65,32 +20,27 @@ export default function Footer(props) {
 
     useEffect(() => {
         configIntercom();
-    }, [intercom?.appId, JSON.stringify(intercom?.boot), auth?.user?.id]);
+    }, [auth && auth?.name]);
     useEffect(() => {
         confgureGtag();
     }, []);
-
-
     const date = new Date();
-    
     return (
         <>
             <Head>
                 {/* Google Analytics now loaded dynamically via lazy loading */}
             </Head>
-
-
             <footer className="bg-[#924DFF] text-white pt-10 pb-3 px-6">
                 <div className="max-w-5xl mx-auto">
                     <div className="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-8 text-sm">
                         <div>
                             <h3 className="font-gulfs text-light text-lg md:text-3xl md:mb-4"> LEGAL </h3>
                             <ul className="space-y-2 font-poppins cursor-pointer">
-                                <li className="fading"> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=88583b44-9385-430c-aa79-3c41dc8a167e" target="blank" > Privacy policy </a> </li>
-                                <li className="fading"> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=f11eb44f-4ddd-4d59-86d1-34c11e3fa80e" target="blank" > Cookies Policy </a> </li>
-                                <li className="fading"> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=a1f91da2-10e4-49e8-88b6-fc716b2645ba" target="blank" > Return Policy </a> </li>
-                                <li className="fading"> <a href="https://ucarecdn.com/6b25399f-e259-4c19-bbdf-70308c5814ef/SPTERMSFINALVMAY2025.pdf" target="blank" > Terms & Conditions </a> </li>
-                                <li className="fading"> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=9a437e57-fcc7-439f-a7e7-96b493a8c50f" target="blank" > Acceptable Use Policy </a> </li>
+                                <li> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=88583b44-9385-430c-aa79-3c41dc8a167e" target="blank" > Privacy policy </a> </li>
+                                <li> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=f11eb44f-4ddd-4d59-86d1-34c11e3fa80e" target="blank" > Cookies Policy </a> </li>
+                                <li> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=a1f91da2-10e4-49e8-88b6-fc716b2645ba" target="blank" > Return Policy </a> </li>
+                                <li> <a href="https://ucarecdn.com/6b25399f-e259-4c19-bbdf-70308c5814ef/SPTERMSFINALVMAY2025.pdf" target="blank" > Terms & Conditions </a> </li>
+                                <li> <a href="https://app.termly.io/policy-viewer/policy.html?policyUUID=9a437e57-fcc7-439f-a7e7-96b493a8c50f" target="blank" > Acceptable Use Policy </a> </li>
                             </ul>
                         </div>
 
@@ -99,10 +49,10 @@ export default function Footer(props) {
                                 GENERAL
                             </h3>
                             <ul className="space-y-2 font-poppins cursor-pointer">
-                                <li className="fading"> <a target="_blank" href="https://blog.spennypiggy.co"> Blog </a> </li>
-                                <li className="fading"> <a target="_blank" href="https://app.termly.io/notify/88583b44-9385-430c-aa79-3c41dc8a167e"> DSAR Form </a> </li>
-                                <li className="fading"> <a target="_blank" href="https://app.termly.io/policy-viewer/policy.html?policyUUID=364c168c-44ab-467e-a98a-a22629fc31f8"> Disclaimer </a> </li>
-                                <li className="fading"> <a href="#" className="termly-display-preferences" > Consent Preferences </a> </li>
+                                <li> <a target="_blank" href="https://blog.spennypiggy.co"> Blog </a> </li>
+                                <li> <a target="_blank" href="https://app.termly.io/notify/88583b44-9385-430c-aa79-3c41dc8a167e"> DSAR Form </a> </li>
+                                <li> <a target="_blank" href="https://app.termly.io/policy-viewer/policy.html?policyUUID=364c168c-44ab-467e-a98a-a22629fc31f8"> Disclaimer </a> </li>
+                                <li> <a href="#" className="termly-display-preferences" > Consent Preferences </a> </li>
                             </ul>
                         </div>
 
@@ -111,9 +61,9 @@ export default function Footer(props) {
                                 HELP
                             </h3>
                             <ul className="space-y-2 font-poppins cursor-pointer">
-                                <li className="fading"> <a href="#" className="livechat" onClick={(e) => { e.preventDefault(); if (window.Intercom) { window.Intercom('show'); } }}> Live Chat</a> </li>
-                                <li className="fading"> <a target="_blank" href="https://intercom.help/spenny-piggy" > FAQ's </a> </li>
-                                <li className="fading"> <Link href={route("promotion-terms")}> Promotion Terms </Link> </li>
+                                <li> <a target="_blank" href="https://spennypiggy.co" className="livechat intercom-dud02y e11rlguj1" > Live Chat</a> </li>
+                                <li> <a target="_blank" href="https://intercom.help/spenny-piggy" > FAQ's </a> </li>
+                                <li> <Link href={route("promotion-terms")}> Promotion Terms </Link> </li>
                             </ul>
                         </div>
 
@@ -122,12 +72,12 @@ export default function Footer(props) {
                                 CONTACT
                             </h3>
                             <ul className="space-y-2 font-poppins cursor-pointer">
-                                <li className="fading"> <a href="tel:02033552057"> 020 3355 2057</a> </li>
-                                <li className="fading">
+                                <li> <a href="tel:02033552057"> 020 3355 2057</a> </li>
+                                <li>
                                 <a href="mailto:support@spennypiggy.co">
                                 support@spennypiggy.co
                                 </a></li>
-                                <li className="fading">55 Colmore Row, B3 2AA</li>
+                                <li>55 Colmore Row, B3 2AA</li>
                             </ul>
                         </div>
                     </div>
@@ -144,12 +94,12 @@ export default function Footer(props) {
                                 className="cursor-pointer"
                             />
                         </Link>
-                        <p className="text-sm font-poppins ">
+                        <p className="text-sm font-poppins">
                             Copyright © {date && date.getFullYear()} Spenny Piggy
                         </p>
                     </div>
 
-                    <p className=" border-t border-white text-center text-xs mt-4 pt-4 px-4 md:px-20 text-white/80 font-poppins">
+                    <p className="border-t border-white text-center text-xs mt-4 pt-4 px-4 md:px-20 text-white/80 font-poppins">
                         All trademarks, logos and brand names are the property
                         of their respective owners. All company, product and
                         service names used in this website are for
