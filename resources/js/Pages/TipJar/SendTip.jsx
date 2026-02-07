cb330b68112d988414b34407221f9816ee53b721
Naveen Tehrpariya <naveentehrpariya@MacBook-Air-2.local>
2026-02-07 12:32:30 +0530

    merge

--- resources/js/Pages/TipJar/SendTip.jsx
+++ resources/js/Pages/TipJar/SendTip.jsx
@@ -5,7 +5,7 @@ export default function SendTip({auth}) {
     const [close, setClose] = useState();
     return (
         <>
-            <Popup hidecontrols={true} bodyclassName=' bg-voilet' modalclass="pinkmodals basic full" space="0" size="md"
+            <Popup hidecontrols={true} bodyclass=' bg-voilet' modalclass="pinkmodals basic full" space="0" size="md"
             action={close} classes={`uppercase text-sm btn-shadow font-gulfs rounded-full px-4 pt-[10px] pb-[7px] pinkbg text-white sfillbankbtn ${auth?.user ? 'loggedin' : 'logout'} `}
             text={`Support Me`} >
                 <TipInner idd={2} />
